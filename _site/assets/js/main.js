35fc2f8a693330f8603c480ce1ebf0c051c2c2de
Olivia <cozymonster29@gmail.com>
2019-03-13 17:11:54 -0700

    first commit

---
+++ _site/assets/js/main.js
@@ -0,0 +1,20 @@
+var jokes;
+
+function getRandomJoke(joke) {
+	console.log('new joke')
+	var joke = jokes[Math.floor(Math.random() * jokes.length)];
+	$('#joke-title').text(joke.title);
+	$('#joke-text').text(joke.joke);
+}
+
+jQuery(window).on("load", function(){
+    $.getJSON('https://api.sheety.co/ea92d66f-0056-4607-88b1-33fdd65f4e0d', function(data) {
+		jokes = data;
+		getRandomJoke()
+	})
+});
+
+$('#new-joke').on("click",function(){
+	console.log('a clicked')
+  getRandomJoke()
+})
