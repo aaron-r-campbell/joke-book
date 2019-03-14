da0a50a7878edeaec374db5f114f6b94b38940b3
Olivia <cozymonster29@gmail.com>
2019-03-13 19:43:37 -0700

    autoscroll to top

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,10 +1,12 @@
 var jokes;
+var scroll = new SmoothScroll('a[href*="#"]');
 
 function getRandomJoke(joke) {
 	console.log('new joke')
 	var joke = jokes[Math.floor(Math.random() * jokes.length)];
 	$('#joke-title').text(joke.title);
 	$('#joke-text').text(joke.joke);
+	scroll.animateScroll(0);
 }
 
 jQuery(window).on("load", function(){
